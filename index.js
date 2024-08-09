9c2c3426ab2e0e79c1179419e2df3e14664b148d
codebypaulina <paulinaczeschla@web.de>
2024-08-09 14:59:22 +0200

    added js function to bookmark icon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const bodyElement = document.querySelector('[data-js="body"]');
+/******************************** DARK MODE  ********************************
 
+const bodyElement = document.querySelector('[data-js="body"]');
 const darkModeButton = document.querySelector('[data-js="button-dark-mode"]');
 const darkModeSlider = document.querySelector('[data-js="slider"]');
 
@@ -7,5 +8,25 @@ darkModeButton.addEventListener("click", () => {
   darkModeButton.classList.toggle("active");
   darkModeSlider.classList.toggle("active");
   bodyElement.classList.toggle("dark");
-  console.log("Button clicked!");
 });
+
+/***************************************************************************/
+
+/****************************** BOOKMARK ICON ******************************/
+
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
+
+bookmarkButton.addEventListener("click", () => {
+  const bookmarkIconSrc = bookmarkIcon.src;
+
+  if (bookmarkIconSrc.endsWith("bookmark.png")) {
+    bookmarkIcon.src = "./img/bookmark_filled.png"; // warum muss ich hier trotzdem "bookmarkIcon.src" schreiben, obwohl oben in der Funktion "bookmarkIconSrc" definiert wurde?
+    return;
+  }
+  bookmarkIcon.src = "./img/bookmark.png";
+});
+
+/***************************************************************************/
+
+/****************************** ANSWER BUTTON ******************************/
