03e270baf562da7b41bc446d9b571c86a89fd80d
codebypaulina <paulinaczeschla@web.de>
2024-08-11 00:51:41 +0200

    added form field text counter

--- form.js
+++ form.js
@@ -1,3 +1,5 @@
+/******************************** CREATE CARDS  ********************************/
+
 const form = document.querySelector('[data-js="form"]');
 const newCardContainer = document.querySelector(
   '[data-js="new-card-container"]'
@@ -15,7 +17,7 @@ form.addEventListener("submit", (event) => {
   const answer = data.answer;
   const tag = data.tag;
 
-  /**************************** DOM elements for card ****************************/
+  // ------------- DOM elements for card ------------- //
   const liElementCard = document.createElement("li");
   liElementCard.classList.add("card");
 
@@ -34,7 +36,7 @@ form.addEventListener("submit", (event) => {
 
   const questionElement = document.createElement("h3");
   questionElement.classList.add("card-question");
-  questionElement.textContent = question; // ----------------------------------- QUESTION
+  questionElement.textContent = question; // ---------------------- QUESTION
   liElementCard.appendChild(questionElement);
 
   const answerButton = document.createElement("button");
@@ -48,7 +50,7 @@ form.addEventListener("submit", (event) => {
   const answerElement = document.createElement("p");
   answerElement.classList.add("card-answer", "hidden");
   answerElement.setAttribute("data-js", "card-answer"); // data-js nötig?
-  answerElement.textContent = answer; // ----------------------------------------- ANSWER
+  answerElement.textContent = answer; // ----------------------------- ANSWER
   liElementCard.appendChild(answerElement);
 
   const ulElementCategories = document.createElement("ul");
@@ -56,8 +58,74 @@ form.addEventListener("submit", (event) => {
   liElementCard.appendChild(ulElementCategories);
 
   const categoryItem = document.createElement("li");
-  categoryItem.textContent = tag; // ------------------------------------------------ TAG
+  categoryItem.textContent = tag; // ------------------------------------ TAG
   ulElementCategories.appendChild(categoryItem);
 
   newCardContainer.appendChild(liElementCard);
 });
+
+/*******************************************************************************/
+/*******************************************************************************/
+
+/********************************* TEXT COUNTER ********************************/
+/************************************ LONG *************************************
+
+// ------ QUESTION
+const questionElement = document.querySelector('[data-js="input-question"]');
+const maxLengthQuestion = questionElement.getAttribute("maxlength");
+const amountLeftQuestion = document.querySelector(
+  '[data-js="amount-left-question"]'
+);
+
+const updateAmountLeftQuestion = (value) => {
+  amountLeftQuestion.innerText = value;
+};
+
+updateAmountLeftQuestion(maxLengthQuestion);
+
+questionElement.addEventListener("input", () => {
+  updateAmountLeftQuestion(maxLengthQuestion - questionElement.value.length);
+});
+
+// ------ ANSWER
+const answerElement = document.querySelector('[data-js="input-answer"]');
+const maxLengthAnswer = answerElement.getAttribute("maxlength");
+const amountLeftAnswer = document.querySelector(
+  '[data-js="amount-left-answer"]'
+);
+
+const updateAmountLeftAnswer = (value) => {
+  amountLeftAnswer.innerText = value;
+};
+
+updateAmountLeftAnswer(maxLengthAnswer);
+
+answerElement.addEventListener("input", () => {
+  updateAmountLeftAnswer(maxLengthAnswer - answerElement.value.length);
+});
+
+/*******************************************************************************/
+/********************************* TEXT COUNTER ********************************/
+/************************************ SHORT ************************************/
+
+const updateAmountLeft = (inputType) => {
+  const inputElement = document.querySelector(`[data-js="input-${inputType}"]`);
+  const amountLeftElement = document.querySelector(
+    `[data-js="amount-left-${inputType}"]`
+  );
+  const maxLength = inputElement.getAttribute("maxlength");
+
+  // Anzeige maxlength
+  amountLeftElement.innerText = maxLength;
+
+  // Eventlistener
+  inputElement.addEventListener("input", () => {
+    amountLeftElement.innerText = maxLength - inputElement.value.length;
+  });
+};
+
+// characters left aktualisieren
+updateAmountLeft("question");
+updateAmountLeft("answer");
+
+/*******************************************************************************/
